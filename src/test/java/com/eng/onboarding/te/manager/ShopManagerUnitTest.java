38b120585e7992b0679191942347988de5a16bc5
Jahid Shohel <jahid.shohel@ingka.com>
2023-08-05 09:45:30 +0200

    Added unit test

---
+++ src/test/java/com/eng/onboarding/te/manager/ShopManagerUnitTest.java
@@ -0,0 +1,32 @@
+package com.eng.onboarding.te.manager;
+
+import com.eng.onboarding.te.Application;
+import com.eng.onboarding.te.domain.Shop;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest(classes = Application.class)
+public class ShopManagerUnitTest {
+    @MockBean
+    private ShopRepository shopRepository;
+    @Autowired
+    private ShopManager shopManager;
+
+    @Test
+    public void createShopTest() {
+        Mockito.when(shopRepository.save(any(Shop.class))).thenAnswer(invocation -> {
+            Shop shop = invocation.getArgument(0, Shop.class);
+            shop.setId(777l);
+            return shop;
+        });
+
+        shopManager.createShop("Shop1", "CountryCode1");
+        verify(shopRepository).save(any(Shop.class));
+    }
+}
