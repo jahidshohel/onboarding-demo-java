dac3821bc693e96e6619ab918629db19ddc8de68
Jahid Shohel <jahid.shohel@ingka.com>
2023-08-05 09:46:35 +0200

    Added integration test

---
+++ src/test/java/com/eng/onboarding/te/manager/ShopManagerIntegrationTest.java
@@ -0,0 +1,25 @@
+package com.eng.onboarding.te.manager;
+
+import com.eng.onboarding.te.Application;
+import com.eng.onboarding.te.domain.Coordinate;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = Application.class)
+public class ShopManagerIntegrationTest {
+    @Autowired
+    private ShopManager shopManager;
+
+    @Test
+    public void getCoordinateTest() throws URISyntaxException, IOException, InterruptedException, ShopNotFoundException {
+        //Coordinate coordinate = shopManager.getCoordinate(60009l);
+        Coordinate coordinate = shopManager.getCoordinate(609L);
+        assertNotNull(coordinate);
+    }
+}
