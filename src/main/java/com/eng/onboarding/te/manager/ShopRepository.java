7db92519b89a2a9c3abe48f8a7792ad0bd085a7e
Jahid Shohel <jahid@Jahids-MBP.lan>
2023-09-11 09:30:00 +0200

    Removed spring depndency

--- src/main/java/com/eng/onboarding/te/manager/ShopRepository.java
+++ src/main/java/com/eng/onboarding/te/manager/ShopRepository.java
@@ -1,11 +1,30 @@
 package com.eng.onboarding.te.manager;
 
 import com.eng.onboarding.te.domain.Shop;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-@Repository
-@Transactional
-public interface ShopRepository extends CrudRepository<Shop, Long> {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Random;
+
+public class ShopRepository {
+
+    private final Map<Long, Shop> inMemoryStorage = new HashMap<>();
+
+    public Shop save(Shop shop) {
+        Long newId = generateId();
+        shop.setId(newId);
+        return inMemoryStorage.put(newId, shop);
+    }
+
+    public Shop findById(Long id) {
+        return inMemoryStorage.get(id);
+    }
+
+    private Long generateId() {
+        long id = new Random().nextLong();
+        while (inMemoryStorage.containsKey(id)) {
+            id = new Random().nextLong();
+        }
+        return id;
+    }
 }
